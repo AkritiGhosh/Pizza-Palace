14e2239ffc7dc172dbb8aaf62fce7a88f40162f7
Akriti Ghosh <akritighosh98@gmail.com>
2024-08-04 10:50:03 +0530

    setup routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-function App() {
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { checkAuth } from "./lib/core";
+import MainPage from "./page/user/MainPage";
+import ComingSoon from "./page/user/ComingSoon";
+import PageNotFound from "./page/user/404";
 
-  return (
-    <>
-    </>
-  )
+function App() {
+  type RouteType = {
+    path: string;
+    element: JSX.Element;
+    loader?: any;
+    action?: any;
+  };
+
+  const privateRoutes: RouteType[] = [
+    { path: "/", element: <MainPage /> },
+    { path: "*", element: <PageNotFound /> },
+  ];
+  const publicRoutes: RouteType[] = [
+    { path: "/", element: <ComingSoon /> },
+    { path: "*", element: <PageNotFound /> },
+  ];
+
+  const router = createBrowserRouter(
+    checkAuth() ? privateRoutes : publicRoutes
+  );
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
