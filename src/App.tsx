fb3c95cf76196e6d5646a89f7ee927d30f0101f4
Akriti Ghosh <akritighosh98@gmail.com>
2024-08-04 13:02:27 +0530

    setup theme and context

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { lazy, useState } from "react";
 import { checkAuth } from "./lib/core";
-import MainPage from "./page/user/MainPage";
-import ComingSoon from "./page/user/ComingSoon";
-import PageNotFound from "./page/user/404";
+import { UserContext } from "./lib/context";
+const MainPage = lazy(() => import("./page/user/MainPage"));
+const ComingSoon = lazy(() => import("./page/user/ComingSoon"));
+const PageNotFound = lazy(() => import("./page/user/404"));
+
+type RouteType = {
+  path: string;
+  element: JSX.Element;
+  loader?: any;
+  action?: any;
+};
+
+const privateRoutes: RouteType[] = [
+  { path: "/", element: <MainPage /> },
+  { path: "*", element: <PageNotFound /> },
+];
+
+const publicRoutes: RouteType[] = [
+  { path: "/", element: <ComingSoon /> },
+  { path: "*", element: <PageNotFound /> },
+];
 
 function App() {
-  type RouteType = {
-    path: string;
-    element: JSX.Element;
-    loader?: any;
-    action?: any;
-  };
-
-  const privateRoutes: RouteType[] = [
-    { path: "/", element: <MainPage /> },
-    { path: "*", element: <PageNotFound /> },
-  ];
-  const publicRoutes: RouteType[] = [
-    { path: "/", element: <ComingSoon /> },
-    { path: "*", element: <PageNotFound /> },
-  ];
-
+  const [darkTheme, setDarkTheme] = useState<boolean>(true);
   const router = createBrowserRouter(
     checkAuth() ? privateRoutes : publicRoutes
   );
-  return <RouterProvider router={router} />;
+  return (
+    <UserContext.Provider value={{ darkTheme, setDarkTheme }}>
+      <RouterProvider router={router} />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
