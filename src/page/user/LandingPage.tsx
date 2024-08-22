1f35d33292c39d16394f98dc8602dd59f38f11a5
Akriti Ghosh <akritighosh98@gmail.com>
2024-08-22 11:39:56 +0530

    dark theme per tailwind

--- src/page/user/LandingPage.tsx
+++ src/page/user/LandingPage.tsx
@@ -1,18 +1,12 @@
-import { useContext } from "react";
-import { UserContext } from "../../lib/context";
 import { Link } from "react-router-dom";
 
 const LandingPage = () => {
-  const { theme } = useContext(UserContext);
-  const bgURL = theme ? "src/assets/dark_bg.jpg" : "src/assets/light_bg.jpg";
-
   return (
     <main
-      style={{ backgroundImage: `url(${bgURL})` }}
-      className="w-full h-screen bg-cover snap-y snap-mandatory overflow-auto"
+      className={`w-full h-screen bg-cover overflow-auto bg-[url("src/assets/light_bg.jpg")] dark:bg-[url("src/assets/dark_bg.jpg")]`}
     >
-      <div className="w-2/5 h-full ml-4 md:ml-[5%] flex flex-col gap-5 justify-center text-right">
-        <h1>
+      <div className="w-2/5 h-full ml-4 md:ml-[5%] flex flex-col gap-5 justify-center text-right text-black dark:text-white">
+        <h1 className="text-7xl font-serif font-semibold dark:drop-shadow-light drop-shadow-dark">
           Fall in love with <br /> Delicious Pizza
         </h1>
         <button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 export default {
   content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        dark: "0 0px 15px rgba(0, 0, 0, 0.25)",
+        light: "0 0px 15px rgba(255, 255, 255, 0.25)",
+      },
+    },
   },
   plugins: [],
 };
-
