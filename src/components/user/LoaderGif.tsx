afb677d9c2de89454a0223d76393d1bee50c1965
Akriti Ghosh <akritighosh98@gmail.com>
2024-08-22 12:05:21 +0530

    Loader gif changed

--- src/components/user/LoaderGif.tsx
+++ src/components/user/LoaderGif.tsx
@@ -2,22 +2,16 @@ import React from "react";
 
 const LoaderGif = () => {
   return (
-      <div className="w-screen h-screen fixed -z-10 top-0 left-0 bg-white bg-opacity-60 flex items-center justify-center pointer-events-none" >
-      <div className="w-1/4  h-auto relative z-50 ">
+    <div className="w-screen h-screen fixed z-10 top-0 left-0 bg-gray-100 dark:bg-gray-800 bg-opacity-80 dark:bg-opacity-80 flex items-center justify-center pointer-events-none">
+      <div className="w-1/4  h-full relative z-50 ">
         <iframe
-          src="https://giphy.com/embed/yB6Jx4t4dov4O88qF1"
+          src="https://giphy.com/embed/eP7hei0yHQxzO"
           width="100%"
           height="100%"
-          className="position:absolute"
           frameBorder="0"
           allowFullScreen
         />
       </div>
-      <p>
-        <a href="https://giphy.com/stickers/daybreak-studio-transparent-yB6Jx4t4dov4O88qF1">
-          via GIPHY
-        </a>
-      </p>
     </div>
   );
 };
