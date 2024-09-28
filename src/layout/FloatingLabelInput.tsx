f51418067fcd0fd2c0ee1b2c87fe23bd7c8dc980
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-28 23:51:57 +0530

    input styling

--- src/layout/FloatingLabelInput.tsx
+++ src/layout/FloatingLabelInput.tsx
@@ -3,28 +3,39 @@ import React from "react";
 type InputProps = {
   id: string;
   name: string;
+  type: string;
+  label: string;
   value: string;
   setValue: (value: string | ((prevVar: string) => string)) => void;
 };
 
-const FloatingLabelInput = ({ id, name, value, setValue, ...props }: InputProps) => {
+const FloatingLabelInput = ({
+  id,
+  name,
+  value,
+  label,
+  type = "text",
+  setValue,
+  ...props
+}: InputProps) => {
   return (
     <div id={id} className="w-full relative">
       <input
+        type={type}
         name={name}
         value={value}
         onChange={(e) => setValue(e.target.value)}
         required
-        className="w-full h-10 rounded-lg text-base px-2 bg-transparent border-2 border-black focus:border-amber-500 !outline-0 peer"
+        className="w-full h-10 rounded-lg text-base px-2 bg-transparent border-2 border-gray-300 focus:border-amber-500 !outline-0 peer"
         {...props}
       />
       <label
-        className="text-base absolute left-2 top-2/4 -translate-y-1/2 pointer-events-none duration-300 
+        className="text-base text-gray-500 absolute left-2 top-2/4 -translate-y-1/2 pointer-events-none duration-300 
           peer-focus:text-xs peer-focus:top-0 peer-focus:px-1 peer-focus:bg-white 
-          peer-valid:text-xs peer-valid:top-0 peer-valid:px-1 peer-valid:bg-white"
+          peer-valid:text-xs peer-valid:top-0 peer-valid:px-1 peer-valid:bg-white peer-focus:text-amber-800 "
         htmlFor="floating_label_input"
       >
-        Enter value here
+        {label}
       </label>
     </div>
   );
