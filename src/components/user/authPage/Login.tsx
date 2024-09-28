85ef7ea8461c79bf6a9bc7c628136785cad9919d
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-28 23:51:37 +0530

    login form ui

--- src/components/user/authPage/Login.tsx
+++ src/components/user/authPage/Login.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import FloatingLabelInput from "../../../layout/FloatingLabelInput";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
   return (
-    <div>Login</div>
-  )
-}
+    <div className="w-full flex flex-col gap-4">
+      <FloatingLabelInput
+        type="email"
+        label="Enter your email"
+        name="email"
+        id="email"
+        value={email}
+        setValue={setEmail}
+      />
+      <FloatingLabelInput
+        type="password"
+        label="Enter your password"
+        name="password"
+        id="password"
+        value={pwd}
+        setValue={setPwd}
+      />
+      <button className="w-auto h-12 py-2 px-4 rounded-md text-base bg-amber-800 text-white font-bold hover:bg-amber-700">
+        Log in
+      </button>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
