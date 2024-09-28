2b46df3cf2c347684c3889884faac64b53d18155
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-28 12:40:31 +0530

    authentication page - with tabs

--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -3,14 +3,16 @@ import Header from "../components/consumer/header/Header";
 
 type LayoutProps = {
   children: ReactNode;
+  withHeader: boolean;
+  className: string
 };
 
-const PageLayout = ({ children }: LayoutProps) => {
+const PageLayout = ({ children, withHeader = true, className='' }: LayoutProps) => {
   return (
-    <>
-      <Header />
+    <div className={"w-screen h-screen overflow-y-auto scroll-thin "+ className}>
+      {withHeader && <Header />}
       {children}
-    </>
+    </div>
   );
 };
 
--- src/page/user/AuthPage.tsx
+++ src/page/user/AuthPage.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useState } from "react";
+import PageLayout from "../../layout/PageLayout";
 
+type authAction = "login" | "register";
 const AuthPage = () => {
-  return (
-    <div>AuthPage</div>
-  )
-}
+  const [action, setAction] = useState<authAction>("login");
 
-export default AuthPage
+  return (
+    <PageLayout
+      className="flex items-center justify-center p-4"
+      withHeader={false}
+    >
+      <div className="relative flex flex-col w-full md:w-1/2 lg:w-3/5 p-4 border border-slate-400 rounded-md">
+        <div className="w-full h-12 top-0 flex flex-row">
+          <button
+            onClick={() => setAction("login")}
+            className={
+              action == "login"
+                ? "w-full border-b-2 border-amber-400 hover:border-slate-200/50 "
+                : "w-full border-b-2 border-transparent hover:border-slate-200/50 "
+            }
+          >
+            Login
+          </button>
+          <button
+            onClick={() => setAction("register")}
+            className={
+              action == "register"
+                ? "w-full border-b-2 border-amber-400 hover:border-slate-200/50 "
+                : "w-full border-b-2 border-transparent hover:border-slate-200/50 "
+            }
+          >
+            Sign up
+          </button>
+        </div>
+      </div>
+    </PageLayout>
+  );
+};
+
+export default AuthPage;
