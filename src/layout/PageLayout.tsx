0d2b843a34f1129989049733c547de0a230363ee
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-29 23:25:20 +0530

    bg and dark mode bg added for layout

--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -3,13 +3,22 @@ import Header from "../components/consumer/header/Header";
 
 type LayoutProps = {
   children: ReactNode;
-  withHeader: boolean;
-  className: string
+  withHeader?: boolean;
+  className: string;
 };
 
-const PageLayout = ({ children, withHeader = true, className='' }: LayoutProps) => {
+const PageLayout = ({
+  children,
+  withHeader = true,
+  className = "",
+}: LayoutProps) => {
   return (
-    <div className={"w-screen h-screen overflow-y-auto scroll-thin "+ className}>
+    <div
+      className={
+        "w-screen h-screen overflow-y-auto scroll-thin flex items-center justify-center p-4 bg-white dark:bg-slate-900 " +
+        className
+      }
+    >
       {withHeader && <Header />}
       {children}
     </div>
